9c93514a3ac2d3f998d6ca62574db1130b58f68e
Hasan Md Nurullah <59159165+nurullah7733@users.noreply.github.com>
2022-01-29 00:48:22 +0600

    Delete src/redux directory

--- src/redux/services/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import counterReducer from "./counterReducer";
-import httpRequest from "./userReducer";
-
-export default combineReducers({
-  counterReducer,
-  httpRequest,
-});
